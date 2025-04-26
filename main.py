d98c91f6c73a82943916dbf25ad3f50f91d85fbf
coding-beagle <nicholas.teague@focusrite.com>
2025-04-26 18:31:06 +0100

    update

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import spidev
-
-spi = spidev.SpiDev()
-
-spi.open(0, 0)
-
-spi.max_speed_hz = 2 ** 20
-
-spi.mode = 0b01
+import click
 
 
-send_data = [int(input("Enter byte to send: ").strip())]
-print(str(send_data))
-print(spi.xfer2(send_data))
+def create_SPI() -> spidev.SpiDev:
+    spi = spidev.SpiDev()
+
+    spi.open(0, 0)
+
+    spi.max_speed_hz = 2**20
+
+    spi.mode = 0b01
+
+    return spi
+
+
+@click.group()
+def cli1():
+    pass
+
+
+@cli1.command()
+# @click.option("--get", help="Command to send")
+def led_on():
+    """Send a command to turn the LED on over SPI"""
+    spi_instance = create_SPI()
+    spi_instance.xfer2(0x20)
+
+
+@cli1.command()
+# @click.option("--get", help="Command to send")
+def led_off():
+    """Send a command to turn the LED off over SPI"""
+    spi_instance = create_SPI()
+    spi_instance.xfer2(0x10)
+
+
+cli = click.CommandCollection(sources=[cli1])
+if __name__ == "__main__":
+    cli()
