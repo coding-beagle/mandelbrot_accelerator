ec5328c395df9f43f28a5463b2f1bdcf6301fb15
coding-beagle <nicholas.teague@focusrite.com>
2025-04-29 23:07:02 +0100

    update progress updater

--- main.py
+++ main.py
@@ -229,8 +229,8 @@ def draw_mandelbrot():
     spi_instance = create_SPI()
     image_data = np.zeros((1024, 512, 3), dtype=np.uint8)
 
-    for x in range(1024):
-        for y in range(512):
+    for y in range(512):
+        for x in range(1024):
             iteration_count = min(
                 get_iteration_count_helper(spi_instance, f"{x},{y}"), 255
             )
