fc91e65bdf69e47b5d7d4f260301efd7494f5dc9
coding-beagle <nicholas.teague@focusrite.com>
2025-04-29 23:03:30 +0100

    change indexing

--- main.py
+++ main.py
@@ -229,7 +229,6 @@ def draw_mandelbrot():
     spi_instance = create_SPI()
     image_data = np.zeros((1024, 512, 3), dtype=np.uint8)
     plt.ion()  # Turn on interactive mode
-    plt.ion()  # Ensure interactive mode is on
     fig, ax = plt.subplots()
     img_display = ax.imshow(image_data, interpolation="nearest")
 
@@ -238,7 +237,7 @@ def draw_mandelbrot():
             iteration_count = min(
                 get_iteration_count_helper(spi_instance, f"{x},{y}"), 255
             )
-            image_data[x, y] = [iteration_count, 0, 0]
+            image_data[y, x] = [iteration_count, 0, 0]
             if y % 10 == 0:  # Update the plot every 10 pixels for better performance
                 img_display.set_data(image_data)
                 fig.canvas.draw_idle()  # Explicitly update the figure canvas
